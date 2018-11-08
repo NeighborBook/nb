f3d9ff50d3d985a25dc41524cbe6801f2111a443
bbjohn <295322187@qq.com>
2018-11-08 12:05:34 +0800

    init

--- partner-weixin-client/src/main/java/com/nb/module/partner/weixin/client/api/message/client/IWeixinMessageClient.java
+++ partner-weixin-client/src/main/java/com/nb/module/partner/weixin/client/api/message/client/IWeixinMessageClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "token", url = WeixinConstant.WEIXIN_URL)
+@FeignClient(name = "message", url = WeixinConstant.WEIXIN_URL)
 @RequestMapping("/cgi-bin")
 public interface IWeixinMessageClient {
 
