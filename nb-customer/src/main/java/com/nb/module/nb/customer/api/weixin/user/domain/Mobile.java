cb40c44ca83fd2d55c1513f80bbe3355c7dc72c7
bbjohn <295322187@qq.com>
2018-11-20 15:23:09 +0800

    init

--- nb-customer/src/main/java/com/nb/module/nb/customer/api/weixin/user/domain/Mobile.java
+++ nb-customer/src/main/java/com/nb/module/nb/customer/api/weixin/user/domain/Mobile.java
@@ -1,6 +1,5 @@
 package com.nb.module.nb.customer.api.weixin.user.domain;
 
-import com.zjk.module.common.authorization.client.api.user.constant.UserConstant;
 import com.zjk.module.common.authorization.client.api.verificationcode.domain.BaseVerificationCodeCheck;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -17,8 +16,5 @@ public class Mobile extends BaseVerificationCodeCheck {
 	@ApiModelProperty(value = "手机")
 	@NotBlank
 	private String mobile;
-
-	@ApiModelProperty(value = "手机是否验证")
-	private Integer mobileVerified = UserConstant.VERIFIED_0;
 
 }
