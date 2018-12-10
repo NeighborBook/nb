28a6b10aa22931d1c0a2ae9c3b55a415e9ef44d4
bbjohn <295322187@qq.com>
2018-12-10 15:02:39 +0800

    init

--- nb-customer/src/main/java/com/nb/module/nb/customer/api/verify/biz/impl/VerifyServiceImpl.java
+++ nb-customer/src/main/java/com/nb/module/nb/customer/api/verify/biz/impl/VerifyServiceImpl.java
@@ -1,6 +1,8 @@
 package com.nb.module.nb.customer.api.verify.biz.impl;
 
 import com.nb.module.nb.customer.api.verify.biz.IVerifyService;
+import com.nb.module.partner.aliyun.dysmsapi.biz.IDySmsApiService;
+import com.nb.module.partner.aliyun.dysmsapi.domain.DySmsApi;
 import com.zjk.module.common.authorization.client.api.verificationcode.client.IVerificationCodeClient;
 import com.zjk.module.common.authorization.client.api.verificationcode.domain.VerificationCode;
 import com.zjk.module.common.authorization.client.api.verificationcode.domain.VerificationCodeCheck;
@@ -21,21 +23,15 @@ public class VerifyServiceImpl extends CommonServiceImpl implements IVerifyServi
 
 	@Autowired
 	private IVerificationCodeClient verificationCodeClient;
-//	@Autowired
-//	private ISMSClient smsClient;
+	@Autowired
+	private IDySmsApiService dySmsApiService;
 
-//	private static final String MAIL_KEYWORD = "mail_{0}";
-//	private static final String MAIL_SUBJECT = "Onlineleasing邮件验证码";
-//	@Value("${verify.mailTemplateCode}")
-//	private String mailTemplateCode;
-//	@Value("${verify.mailEnTemplateCode}")
-//	private String mailEnTemplateCode;
-//
 	private static final String SMS_KEYWORD = "sms_{0}";
-//	@Value("${verify.smsTemplateCode}")
-//	private String smsTemplateCode;
-//	@Value("${verify.smsEnTemplateCode}")
-//	private String smsEnTemplateCode;
+
+	@Value("${verify.signName}")
+	private String signName;
+	@Value("${verify.smsTemplateCode}")
+	private String smsTemplateCode;
 
 	private static String VC = "0123456789";
 
@@ -49,16 +45,11 @@ public class VerifyServiceImpl extends CommonServiceImpl implements IVerifyServi
 	public String sms(String mobile, String lang) {
 		// 生成验证码
 		VerificationCode verificationCode = checkJsonContainer(verificationCodeClient.generate(new VerificationCodeSetting(MessageFormat.format(SMS_KEYWORD, mobile), 6, VC)));
-		// 发送短信 TODO 后续再考虑总线
-		Map<String, String> model = new HashMap<>();
-		model.put("verificationcode", verificationCode.getCode());
-		// TODO
-		if ("en-us".equalsIgnoreCase(lang)) {
-			//checkJsonContainer(smsClient.sendByTemplate(new com.sbm.module.common.message.api.sms.domain.SendByTemplate(mobile, null, new Date(), smsEnTemplateCode, model)));
-		} else {
-			//checkJsonContainer(smsClient.sendByTemplate(new com.sbm.module.common.message.api.sms.domain.SendByTemplate(mobile, null, new Date(), smsTemplateCode, model)));
-		}
-		log.info(verificationCode.getCode());
+		// 发送短信 TODO 后续再考虑总线，改造成message形式，目前先简单弄
+		Map<String, Object> model = new HashMap<>();
+		model.put("code", verificationCode.getCode());
+		dySmsApiService.sendSMS(new DySmsApi(mobile, signName, smsTemplateCode, model, null));
+//		log.info(verificationCode.getCode());
 		// 返回键
 		return verificationCode.getKey();
 	}
--- partner-aliyun-oss/src/main/java/com/nb/module/partner/aliyun/dysmsapi/domain/DySmsApi.java
+++ partner-aliyun-oss/src/main/java/com/nb/module/partner/aliyun/dysmsapi/domain/DySmsApi.java
@@ -1,6 +1,9 @@
 package com.nb.module.partner.aliyun.dysmsapi.domain;
 
+import com.alibaba.fastjson.JSON;
 import lombok.Data;
+
+import java.util.Map;
 
 @Data
 public class DySmsApi {
@@ -14,4 +17,23 @@ public class DySmsApi {
 	private String templateParam;
 
 	private String outId;
+
+	public DySmsApi() {
+	}
+
+	public DySmsApi(String mobile, String signName, String templateCode, Map<String, Object> templateParam, String outId) {
+		this.mobile = mobile;
+		this.signName = signName;
+		this.templateCode = templateCode;
+		this.templateParam = JSON.toJSONString(templateParam);
+		this.outId = outId;
+	}
+
+	public DySmsApi(String mobile, String signName, String templateCode, String templateParam, String outId) {
+		this.mobile = mobile;
+		this.signName = signName;
+		this.templateCode = templateCode;
+		this.templateParam = templateParam;
+		this.outId = outId;
+	}
 }
