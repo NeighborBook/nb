b814dbb793933d288d4c4e70b4426f0b003da780
bbjohn <295322187@qq.com>
2018-12-17 16:12:03 +0800

    init

--- nb-eureka-server/src/main/resources/sh/uat/start.sh
+++ nb-eureka-server/src/main/resources/sh/uat/start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash  
-nohup java -Xmx500m -jar /home/microservice-uat/service/nb-eureka-server/nb-eureka-server-1.0.0.jar --spring.profiles.active='uat, eureka_uat' --eureka.instance.ip-address=47.100.243.160 &
+nohup java -Xmx500m -jar /home/microservice-uat/service/nb-eureka-server/nb-eureka-server-1.0.0.jar --spring.profiles.active='uat' --eureka.instance.ip-address=47.100.243.160 &
