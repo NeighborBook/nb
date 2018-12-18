c54d07d387f9dbcd24dde1960006b9aa0a76f6bf
bbjohn <295322187@qq.com>
2018-12-18 09:52:30 +0800

    init

--- nb-customer/src/main/java/com/nb/module/nb/customer/api/weixin/message/biz/impl/MessageServiceImpl.java
+++ nb-customer/src/main/java/com/nb/module/nb/customer/api/weixin/message/biz/impl/MessageServiceImpl.java
@@ -10,6 +10,7 @@ import com.nb.module.partner.weixin.client.api.message.provider.WeixinMessageTem
 import com.nb.module.partner.weixin.client.api.token.domain.AccessToken;
 import com.zjk.module.common.base.biz.impl.CommonServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.text.SimpleDateFormat;
@@ -26,9 +27,17 @@ public class MessageServiceImpl extends CommonServiceImpl implements IMessageSer
 	@Autowired
 	private ITokenService tokenService;
 
-	public static final String BOOK_LENDING_REMINDER_TEMPLATE_ID = "-LGNf9vuHBh7qksNK5PtommL9bjDkEYMsD6eiOIquPw";
-	public static final String BOOK_LENDING_STATUS_REMINDER_TEMPLATE_ID = "RGHNpgGk5LwDLeGHh25iSuCNGdOm2fcoCl9-Wt_gLdA";
+	@Value("${weixin.message.bookLendingReminderTemplateId}")
+	private String bookLendingReminderTemplateId;
 
+	@Value("${weixin.message.bookLendingReminderTemplateUrl}")
+	private String bookLendingReminderTemplateUrl;
+
+	@Value("${weixin.message.bookLendingStatusReminderTemplateId}")
+	private String bookLendingStatusReminderTemplateId;
+
+	@Value("${weixin.message.bookLendingStatusReminderTemplateUrl}")
+	private String bookLendingStatusReminderTemplateUrl;
 
 	public static final SimpleDateFormat YYYYMMDD = new SimpleDateFormat("yyyy-MM-dd");
 
@@ -41,12 +50,12 @@ public class MessageServiceImpl extends CommonServiceImpl implements IMessageSer
 	@Override
 	public void sendBookLendingReminder(String toUserOpenid, String fromUserName, String bookName, Date startBorrowDate, String orderCode) {
 		WeixinMessageTemplateDataMap data = provider.prepareBorrowData("有人向您提出借书申请！", fromUserName, bookName, YYYYMMDD.format(startBorrowDate), "感谢您的分享！请点击进入详情页确认!");
-		templateSend(provider.prepareTemplate(toUserOpenid, BOOK_LENDING_REMINDER_TEMPLATE_ID, "http://upload.neighborbook.com.cn/order_detail.php?order=" + orderCode, data));
+		templateSend(provider.prepareTemplate(toUserOpenid, bookLendingReminderTemplateId, bookLendingReminderTemplateUrl + orderCode, data));
 	}
 
 	@Override
 	public void sendBookLendingStatusReminder(String toUserOpenid, String bookName, String status, Date date, String orderCode) {
 		WeixinMessageTemplateDataMap data = provider.prepareBorrowData("您的图书有了新的动态！", bookName, status, YYYYMMDD.format(date), "请点击进入详情页处理!");
-		templateSend(provider.prepareTemplate(toUserOpenid, BOOK_LENDING_STATUS_REMINDER_TEMPLATE_ID, "http://upload.neighborbook.com.cn/order_detail.php?order=" + orderCode, data));
+		templateSend(provider.prepareTemplate(toUserOpenid, bookLendingStatusReminderTemplateId, bookLendingStatusReminderTemplateUrl + orderCode, data));
 	}
 }
